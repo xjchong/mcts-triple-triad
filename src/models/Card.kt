d9a84b73ca5402c4d0ec8684bfdbcdfe82fc2aa9
Xavier <xavier.chong.j@gmail.com>
2021-05-06 16:11:52 -0400

    Update cards to use NESW ordering for values

--- src/models/Card.kt
+++ src/models/Card.kt
@@ -1,14 +1,14 @@
 package models
 
 class Card private constructor(val name: String, val rarity: CardRarity,
-                val w: Int, val n: Int, val s: Int, val e: Int,
+                val n: Int, val e: Int, val s: Int, val w: Int,
                 val type: CardType? = null) {
 
     companion object {
-        val Bomb = Card("Bomb", Common, 4, 3, 3, 3)
-        val Coerl = Card("Coerl", Common, 5, 2, 2, 5)
-        val Dodo = Card("Dodo", Common, 4, 4, 3, 2)
+        val Bomb = Card("Bomb", Common, 3, 4, 3, 3)
+        val Coeurl = Card("Coeurl", Common, 2, 5, 2, 5)
+        val Dodo = Card("Dodo", Common, 4, 2, 3, 4)
         val Mandragora = Card("Mandragora", Common, 4, 2, 5, 3)
-        val Sabotender = Card("Sabotender", Common, 3, 4, 3, 3)
+        val Sabotender = Card("Sabotender", Common, 4, 3, 3, 3)
     }
 }
